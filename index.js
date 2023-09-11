ccf21cfc77e4db4188eaead10004e5f77b971b52
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-10 18:28:13 -0700

    questions array filled

--- index.js
+++ index.js
@@ -5,11 +5,67 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command will install project dependencies?",
+        default: 'npm i',
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this project's usage for?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose the appropriate license for your project:",
+        choices: [
+            "MIT",
+            "APACHE 2.0",
+            "GPL 3.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can others contribute to this project?",
+        default: "Please contact me via email or GitHub for more information."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide the tests test command:",
+        default: 'npm test',
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub username:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email:"
+    }
+];
 
 //
 function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 //
@@ -18,7 +74,7 @@ function init() {
         console.log('Your readme.md is being generated...');
         writeToFile("README.md", generateMarkdown(answers));
         console.log('Your readme.md has been generated!');
-      });
+    });
 }
 
 // Function call to initialize app
