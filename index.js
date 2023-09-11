d0794047269cfb8e2c79fc003a7256ca7b21ecfc
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-10 18:17:00 -0700

    init and write to file functions done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-// TODO: Include packages needed for this application
+// 
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+//
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log('Your readme.md is being generated...');
+        writeToFile("README.md", generateMarkdown(answers));
+        console.log('Your readme.md has been generated!');
+      });
+}
 
 // Function call to initialize app
 init();
